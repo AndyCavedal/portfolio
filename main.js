237ce4f8dd8529dd5c109d12767df2f2b773fc96
Andy Cavedal <hcavedal@gmail.com>
2023-06-21 19:21:06 -0300

    [add] responsive hecho, ahora toca hacer menu desplegable

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log('Hello, world!');
+
+// window.addEventListener('resize', function() {
+//     var menu = document.getElementById('menu-logo');
+//     var anchoPantalla = window.innerWidth;
+
+//     if (anchoPantalla <= 500) {
+//         menu.classList.add('display');
+//         console.log('pantalla chica')
+//     } else {
+//         menu.classList.remove('display')
+//         console.log('pantalla grande')
+//     }
+// });
